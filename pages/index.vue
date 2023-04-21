f2e01599145292a646f9516adb5f850212fa4282
Your Name <you@example.com>
2023-04-21 09:49:12 +0900

    테스트 api

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,33 @@
 <template>
-        <div class="index">
-                {{ name }}
-                <base-input></base-input>
-        </div>
+  <div class="index">
+    {{ name }}
+    <input type="text" v-model="input" @input="handelInput">
+  </div>
 </template>
 
 <script lang="ts">
+import { test, IProductS } from '@/store/modules/moduleUser'
 import { Vue, Component, namespace } from 'nuxt-property-decorator'
 
 const moduleUser = namespace('modules/moduleUser');
 
 @Component({})
 export default class Index extends Vue {
-        input =''
+  input = ''
 
-      @moduleUser.Getter('getName')
-      name!: string;
+  @moduleUser.Getter('getName')
+  name!: string;
 
-      @moduleUser.Action('updateName')
-      updateName!: (name : string)=> void
+  @moduleUser.Action('updateName')
+  updateName!: (name: string) => void
 
-      handelInput(){
-        this.updateName(this.input);
-      }
+  handelInput() {
+    this.updateName(this.input);
+  }
+  async mounted() {
+    const res: IProductS = await test();
+    this.input = this.name
+  }
 }
 </script>
 
--- store/modules/moduleUser.ts
+++ store/modules/moduleUser.ts
@@ -1,26 +1,55 @@
-import { Module, VuexModule, Action, Mutation } from "vuex-module-decorators";
+import { Module, VuexModule, Action, Mutation, MutationAction } from "vuex-module-decorators";
+import axios from 'axios'
 
+interface IRating {
+  count: number,
+  rate: number,
+}
+interface IProduct {
+  category: string
+  description: string
+  id: number
+  image: string
+  price: number
+  rating: IRating
+  title: number
+}
+
+export interface IProductS {
+  lists: IProduct[]
+}
+
+export async function test(): Promise<IProductS> {
+  const products: IProductS = (await axios.get("https://fakestoreapi.com/products")).data;
+  return products;
+}
+
+interface Post {
+  name: string
+}
 
 @Module({
-    stateFactory : true,
+  stateFactory: true,
 })
 
-export default class ModuleUser extends VuexModule{
-    //state
-    private name = '';
+export default class ModuleUser extends VuexModule {
+  //state
+  private name = '염대원';
+  private posts: Post[] = []
+  private get getName(): string {
+    return this.name;
+  }
 
-    private get getName() : string{
-        return this.name;
-    }
 
-    
-    @Mutation
-     setName(payload: string ): void{
-        this.name = payload
-    }
+  // @Mutation
+  // setName(payload: string): void {
+  //     this.name = payload
+  // }
 
-    @Action({ commit: 'setName' })
-    public updateName(newName: string): string {
-       return newName;
-    }
+  // @Action({ commit: 'setName' })
+  // public updateName(newName: string): string {
+  //     return newName;
+  // }
+
 }
+
